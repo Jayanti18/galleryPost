e8ce6f55e9f021fb50b5a39087d8d77dd7b40168
jayanti18 <jayantirajesh@yahoo.com>
2019-01-27 17:09:53 -0500

    creating new App GalleryPost

---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CLASS_COLLECTION = "art";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+  
+  /*  "/api/art"
+   *    GET: finds all artist
+   *    POST: create a new artist
+   */
+  
+  app.get("/api/art", function(req, res) {
+    db.collection(CLASS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+          handleError(res, err.message, "Failed to get contacts.");
+        } else {
+          res.status(200).json(docs);
+        }
+      });
+  });
+  
+  app.post("/api/art", function(req, res) {
+    var newArtist = req.body;
+    newArtist.createDate = new Date();
+ 
+    if (!req.body.name) {
+      handleError(res, "Invalid user input", "Must provide a name.", 400);
+    } else {
+      db.collection(CLASS_COLLECTION).insertOne(newArtist, function(err, doc) {
+        if (err) {
+          handleError(res, err.message, "Failed to create new contact.");
+        } else {
+          res.status(201).json(doc.ops[0]);
+        }
+      });
+    }
+  });
+  
+  /*  "/api/art/:id"
+   *    GET: find artist by id
+   *    PUT: update artist by id
+   *    DELETE: deletes artist by id
+   */
+  
+  app.get("/api/art/:id", function(req, res) {
+  });
+  
+  app.put("/api/art/:id", function(req, res) {
+  });
+  
+  app.delete("/api/art/:id", function(req, res) {
+  });
